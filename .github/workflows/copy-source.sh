449ca4d6ad148faab805eb50b5c88f9bebfced5d
Jaromir Benes <jaromir.benes@gmail.com>
2022-09-20 20:58:03 +0200

    Updated

--- .github/workflows/copy-source.sh
+++ .github/workflows/copy-source.sh
@@ -1,6 +1,7 @@
 
 
 cd iris-reference
+pwd
 
 if [[ -d source/ ]]; then
     rm -rf source/
@@ -9,18 +10,21 @@ fi
 mkdir source
 
 cd ../iris-toolbox
+pwd
 
-find . -name '*.md' -o -name '.pages' \
-    | xargs cp --parents -t ../iris-reference/source/
+find . \( -name '*.md' -o -name '.pages' \) -print \
+    | while read -r filename; do cp --parents -t ../iris-reference/source/ "$filename"; done
 
 cd ../iris-reference
+pwd
 
 mv ./source/README.md ./source/index.md
 
 cp -r ./extra ./source/extra
 
-tree -a ./source/ 
+#tree -a ./source/ 
 
 cd ..
+pwd
 
 
